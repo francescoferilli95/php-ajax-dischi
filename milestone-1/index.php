198bb01c6311c165bc561339a766e81f27805da4
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-20 15:05:52 +0200

    milestone 1 completed

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
     <!-- STYLES -->
     <link rel="stylesheet" href="./css/main.css">
     <title>PHP Dischi</title>
@@ -14,7 +17,19 @@
 
     <main>
         <div class="container">
-            <h1>Content here</h1>
+
+            <?php include_once __DIR__ . './partials/database.php'; ?>
+
+                <?php foreach ($database as $disc) {
+                    // var_dump($disc); ?> 
+                    <div class="box">
+                        <img src="<?php echo $disc['poster']; ?>" alt="Disc Poster Img">
+                        <h3><?php echo $disc['title']; ?></h3>
+                        <h4><?php echo $disc['author']; ?></h4>
+                        <h3><?php echo $disc['year']; ?></h3>
+                        <h4><?php echo $disc['genre']; ?></h4>
+                    </div>
+               <?php } ?>
         </div>
     </main>
 
