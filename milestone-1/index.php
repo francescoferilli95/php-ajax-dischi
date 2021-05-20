dea36b7077c8fe2ee400ce2000e39b81983fc17f
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-20 14:34:36 +0200

    creating repository & added starting point

---
+++ milestone-1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- STYLES -->
+    <link rel="stylesheet" href="./css/main.css">
+    <title>PHP Dischi</title>
+</head>
+<body>
+    
+    <?php include_once __DIR__ . './partials/header.php'; ?>
+
+    <main>
+        <div class="container">
+            <h1>Content here</h1>
+        </div>
+    </main>
+
+</body>
+</html>
---
+++ milestone-1/partials/header.php
@@ -0,0 +1,4 @@
+
+<header class="header">
+    <a href="./"><img class="img" src="./img/logo.png" alt="Site Brande Logo"></a>
+</header>
